a4eea33cb6bdc34955e4f561115052d01b1cde0f
AndreLuiz2022 <andreluiz01bhz@gmail.com>
2024-05-19 23:02:53 -0300

    feat: cryptography  password on CreateUser Route

--- app/src/controllers/UserController.ts
+++ app/src/controllers/UserController.ts
@@ -1,37 +1,76 @@
 import { Request, Response } from "express";
 import { user } from "../model/UserModel";
-import { log } from "console";
-import { builtinModules } from "module";
+import bcrypt from "bcrypt";
+
+const saltRounds = Math.floor(Math.random() * (16 - 10 + 1)) + 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+
+interface IUser {
+  _id: string;
+  nick: string;
+  email: string;
+  password: string;
+}
+
+interface IToSendToUser {
+  _id: string;
+  nick: string;
+  email: string;
+}
+
+type TSignInUser = Omit<IUser, "_id" | "nick">;
 
 export class UserController {
-
   static findAllUsers = async (req: Request, res: Response) => {
     try {
       const allUsers = await user.find();
       res.status(200).json(allUsers);
-    } catch (err) { }
+    } catch (err) {}
   };
 
   static getUserByEmail = async (req: Request, res: Response) => {
     const emailUser = req.params.email;
     try {
       const returnedUser = await user.find({ email: emailUser });
-      if (returnedUser.length === 0) { throw "Usuário não encontrado" };
+      if (returnedUser.length === 0) {
+        throw "Usuário não encontrado";
+      }
       res.status(200).json(returnedUser);
     } catch (err) {
       res.status(500).json({ message: err });
     }
-  }
+  };
 
   static createUser = async (req: Request, res: Response) => {
     try {
-      const newUser = req.body;
+      const { email, nick, password }: IUser = req.body;
+      const cryptoPassword = bcrypt.hashSync(password.trim(), salt);
+
+      const thisEmailExists = await user.find({ email });
+      if (thisEmailExists.length === 1) {
+        throw "Email já cadastrado";
+      }
+      if (thisEmailExists.length > 1) {
+        throw "Erro crítico, esse email já foi cadastrado duas vezes.";
+      }
+
+      const newUser = {
+        nick: nick.trim(),
+        email: email.trim(),
+        password: cryptoPassword,
+      };
+
       await user.create(newUser);
-      res.status(200).json({"menssager":"Usuário criado!"});
-    } catch(err) {
 
+      const bdData: Array<IUser> = await user.find({ email });
+
+      const { _id, nick: toSendNick, email: toSendEmail } = bdData[0];
+
+      const userToSend = { _id, nick: toSendNick, email: toSendEmail };
+
+      res.status(200).json({ token: "token vem aqui", user: userToSend });
+    } catch (err) {
+      res.status(500).json(err);
     }
-  }
+  }; 
 }
-  
-
