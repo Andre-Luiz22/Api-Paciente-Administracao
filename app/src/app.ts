e9c44e7c479cb708b00a5a6e0a0b39c872285691
AndreLuiz2022 <andreluiz01bhz@gmail.com>
2024-05-21 21:24:10 -0300

    feat: add Cors to backend

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from "express";
 import { routes } from "./routes";
 import { db } from "../config/dbConnect";
+import cors from "cors";
 
+const options: cors.CorsOptions = {
+  origin: "*",
+  methods: "GET, POST, PATCH",
+};
 export const app = express();
 app.use(express.json());
+
+app.use(cors(options));
 routes(app);
 
 db.on("error", () => console.log("erro de conexão"));
