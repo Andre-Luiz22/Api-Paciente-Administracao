0b11ff08f5f2fdf936c8a17a37785838c653bfde
Madu Xavier <pro.maduxavier@gmail.com>
2024-06-06 18:59:47 -0300

    add idUnit in LessonModel

--- app/src/model/LessonModel.ts
+++ app/src/model/LessonModel.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const LessonSchema = new mongoose.Schema(
   {
     id: { type: mongoose.Schema.Types.ObjectId },
+    idUnit : {type: mongoose.Schema.Types.String},
     texts: { type: mongoose.Schema.Types.Array },
     imgs: { type: mongoose.Schema.Types.Array },
     video: { type: mongoose.Schema.Types.String },
